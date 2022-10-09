5980096cf0c3c59702df3146a74a87c5ecde4073
Soham Sarkar <soham.ss1@gmail.com>
2022-10-09 15:02:59 +0530

    feat: hover states

--- index.js
+++ index.js
@@ -5,12 +5,11 @@ const chart = document.querySelector('.chart')
 const renderChart = () => {
   const max = getHighestExpense(expenses)
 
-  console.log({max})
   const getBase = data => `
   <div class="bar-box">
-    <div class="bar" style="height:${data.amount * 3}px; ${
-    max === data.amount ? `background-color: #76B5BC` : ``
-  }"></div>
+    <div class="bar ${max === data.amount ? 'bar-h' : ''}" data-amount="$${
+    data.amount
+  }" style="height:${data.amount * 3}px;"></div>
     <p class="bar-info">${data.day}</p>
   </div>
   `
@@ -18,6 +17,20 @@ const renderChart = () => {
   const chartHtmlStr = expenses.map(expense => getBase(expense)).join(' ')
 
   chart.innerHTML = chartHtmlStr
+
+  const bars = [...document.querySelectorAll('.bar')]
+
+  bars.forEach(bar =>
+    bar.addEventListener('mouseover', e => {
+      e.target.style.setProperty('--hover-opacity', 1)
+    }),
+  )
+
+  bars.forEach(bar =>
+    bar.addEventListener('mouseout', e => {
+      e.target.style.setProperty('--hover-opacity', 0)
+    }),
+  )
 }
 
 const getHighestExpense = exp => Math.max(...exp.map(e => e.amount))
